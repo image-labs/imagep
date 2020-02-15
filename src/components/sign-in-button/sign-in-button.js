da86da856ff71ea3537e72f052030fb847cf6b52
sreenaths <sree@apache.org>
2020-02-16 00:06:26 +0530

    Change to hide 'with GitHub' text when not hovered

--- src/components/sign-in-button/sign-in-button.js
+++ src/components/sign-in-button/sign-in-button.js
@@ -9,7 +9,7 @@ const signinURL = generateSignInURL();
 function SignInButton() {
   return (
     <a href={signinURL} target="_blank" rel="noopener noreferrer" className="sign-in-button">
-      <i className="fa fa-github"></i> Sign in with GitHub
+      <i className="fa fa-github"></i> Sign in <span className="with-gh">with GitHub</span>
     </a>
   );
 }
